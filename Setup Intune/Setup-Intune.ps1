a0541324b5532c8953c6513b360e69db87ffc40c
Alex Fields <50560275+vanvfields@users.noreply.github.com>
2019-05-24 15:22:23 -0500

    Add files via upload

---
+++ Setup Intune/Setup-Intune.ps1
@@ -0,0 +1,45 @@
+﻿##
+## This script leverages other Microsoft Graph scripts in order to import the JSON files which deploy the baseline policies
+## Before running the script, ensure all scripts and JSON files are present in the same working directory
+## 
+## Enter your admin username (e.g. intuneadmin@itpromentor.com); if the variable is not specified the script will prompt
+## E.g. .\Setup-Intune.ps1 -User intuneadmin@itpromentor.com
+##
+## NOTE: No assignments will be made (policies will be imported but not deployed)
+## To deploy the polices simply assign them to groups
+##
+
+Param (
+    $User   
+)
+if($User -eq $null -or $User -eq ""){
+
+    $User = Read-Host -Prompt "Please specify your user principal name for Intune administration"
+    Write-Host
+}
+
+
+## Import the iOS MDM policies
+.\Import-Compliance.ps1 -ImportPath .\Compliance-iOS.json -User $User
+.\Import-DeviceConfig.ps1 -ImportPath .\Config-iOS.json -User $User
+
+## Import the Android MDM policies
+.\Import-Compliance.ps1 -ImportPath .\Compliance-Android.json -User $User
+.\Import-DeviceConfig.ps1 -ImportPath .\Config-Android.json -User $User
+
+## Import the macOS MDM policies
+.\Import-Compliance.ps1 -ImportPath .\Compliance-macOS.json -User $User
+.\Import-DeviceConfig.ps1 -ImportPath .\Config-macOS.json -User $User
+.\Import-DeviceConfig.ps1 -ImportPath .\Endpoint-macOS.json -User $User
+
+## Import the Windows 10 MDM policies
+.\Import-Compliance.ps1 -ImportPath .\Compliance-Win10.json -User $User
+.\Import-DeviceConfig.ps1 -ImportPath .\Config-Win10ProBasic.json -User $User
+.\Import-DeviceConfig.ps1 -ImportPath .\Config-Win10ProAdvanced.json -User $User
+.\Import-DeviceConfig.ps1 -ImportPath .\Endpoint-Win10ProBasic.json -User $User
+.\Import-DeviceConfig.ps1 -ImportPath .\Endpoint-Win10ProAdvanced.json -User $User
+.\Import-DeviceConfig.ps1 -ImportPath .\Update-Win10.json -User $User
+
+## Import the MAM policies for iOS and Android
+.\Import-MAMPolicy.ps1 -ImportPath .\AppProtection-iOS.json -User $User
+.\Import-MAMPolicy.ps1 -ImportPath .\AppProtection-Android.json -User $User
