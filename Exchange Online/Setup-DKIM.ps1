d713ec075d7eb431586bd61f30c965db1bb22a99
Alex Fields <50560275+vanvfields@users.noreply.github.com>
2019-09-13 11:43:16 -0500

    Update Setup-DKIM.ps1

--- Exchange Online/Setup-DKIM.ps1
+++ Exchange Online/Setup-DKIM.ps1
@@ -28,7 +28,7 @@ if($DomainName -eq $null -or $DomainName -eq ""){
 Write-Host -ForegroundColor $MessageColor "Enter the (Points to) CNAME values in DNS for selector1._domainkey and selector2._domainkey:"
 Write-Host 
 
-Get-DkimSigningConfig $DomainName | fl *cname 
+Get-DkimSigningConfig $DomainName | fl Domain,*cname 
 
 ## Pause the script to allow time for entering DKIM records
 Write-Host  
