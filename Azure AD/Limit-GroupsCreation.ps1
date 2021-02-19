0c6f958280a8f606542d2877b311683d18ad66ce
Alex Fields <50560275+vanvfields@users.noreply.github.com>
2021-02-19 14:20:46 -0700

    Add files via upload

---
+++ Azure AD/Limit-GroupsCreation.ps1
@@ -0,0 +1,53 @@
+﻿
+<##################################################################################################
+#
+.SYNOPSIS
+Define a security group using the variable $GroupName
+This will limit the privilege for creating Office 365 Groups to the specified security group
+
+
+You have to connect to Azure AD using Connect-AzureAD before running this script:
+https://docs.microsoft.com/en-us/powershell/azure/active-directory/install-adv2?view=azureadps-2.0
+
+Source of script:
+https://docs.microsoft.com/en-us/microsoft-365/admin/create-groups/manage-creation-of-groups?view=o365-worldwide
+
+.NOTES
+    FileName:    Limit-GroupsCreation.ps1
+    Author:      Alex Fields, ITProMentor.com
+    Created:     02-01-2020
+	Revised:     02-01-2020
+    Version:     1.0
+    
+#>
+###################################################################################################
+
+
+
+$GroupName = "Group Creators"
+$AllowGroupCreation = "False"
+
+
+$settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+if(!$settingsObjectID)
+{
+	  $template = Get-AzureADDirectorySettingTemplate | Where-object {$_.displayname -eq "group.unified"}
+    $settingsCopy = $template.CreateDirectorySetting()
+    New-AzureADDirectorySetting -DirectorySetting $settingsCopy
+    $settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+}
+
+$settingsCopy = Get-AzureADDirectorySetting -Id $settingsObjectID
+$settingsCopy["EnableGroupCreation"] = $AllowGroupCreation
+
+if($GroupName)
+{
+	$settingsCopy["GroupCreationAllowedGroupId"] = (Get-AzureADGroup -SearchString $GroupName).objectid
+} else {
+$settingsCopy["GroupCreationAllowedGroupId"] = $GroupName
+}
+Set-AzureADDirectorySetting -Id $settingsObjectID -DirectorySetting $settingsCopy
+
+(Get-AzureADDirectorySetting -Id $settingsObjectID).Values
+
+
