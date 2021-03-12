5cd65f39eaef1c2bff7513ce6c2037fdce390e06
Alex Fields <50560275+vanvfields@users.noreply.github.com>
2021-03-12 10:32:12 -0700

    Add files via upload

---
+++ Windows 10/Install-OneDriveProfile.ps1
@@ -0,0 +1,620 @@
+﻿
+###################################################################################################
+## Functions used in the script
+###################################################################################################
+
+function Get-AuthToken {
+
+<#
+.SYNOPSIS
+This function is used to authenticate with the Graph API REST interface
+.DESCRIPTION
+The function authenticate with the Graph API Interface with the tenant name
+.EXAMPLE
+Get-AuthToken
+Authenticates you with the Graph API interface
+.NOTES
+NAME: Get-AuthToken
+#>
+
+[cmdletbinding()]
+
+param
+(
+    [Parameter(Mandatory=$true)]
+    $User
+)
+
+$userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
+
+$tenant = $userUpn.Host
+
+Write-Host "Checking for AzureAD module..."
+
+    $AadModule = Get-Module -Name "AzureAD" -ListAvailable
+
+    if ($AadModule -eq $null) {
+
+        Write-Host "AzureAD PowerShell module not found, looking for AzureADPreview"
+        $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
+
+    }
+
+    if ($AadModule -eq $null) {
+        write-host
+        write-host "AzureAD Powershell module not installed..." -f Red
+        write-host "Install by running 'Install-Module AzureAD' or 'Install-Module AzureADPreview' from an elevated PowerShell prompt" -f Yellow
+        write-host "Script can't continue..." -f Red
+        write-host
+        exit
+    }
+
+# Getting path to ActiveDirectory Assemblies
+# If the module count is greater than 1 find the latest version
+
+    if($AadModule.count -gt 1){
+
+        $Latest_Version = ($AadModule | select version | Sort-Object)[-1]
+
+        $aadModule = $AadModule | ? { $_.version -eq $Latest_Version.version }
+
+            # Checking if there are multiple versions of the same module found
+
+            if($AadModule.count -gt 1){
+
+            $aadModule = $AadModule | select -Unique
+
+            }
+
+        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+
+    }
+
+    else {
+
+        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+
+    }
+
+[System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
+
+[System.Reflection.Assembly]::LoadFrom($adalforms) | Out-Null
+
+$clientId = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547"
+
+$redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+
+$resourceAppIdURI = "https://graph.microsoft.com"
+
+$authority = "https://login.microsoftonline.com/$Tenant"
+
+    try {
+
+    $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
+
+    # https://msdn.microsoft.com/en-us/library/azure/microsoft.identitymodel.clients.activedirectory.promptbehavior.aspx
+    # Change the prompt behaviour to force credentials each time: Auto, Always, Never, RefreshSession
+
+    $platformParameters = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList "Auto"
+
+    $userId = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier" -ArgumentList ($User, "OptionalDisplayableId")
+
+    $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI,$clientId,$redirectUri,$platformParameters,$userId).Result
+
+        # If the accesstoken is valid then create the authentication header
+
+        if($authResult.AccessToken){
+
+        # Creating header for Authorization token
+
+        $authHeader = @{
+            'Content-Type'='application/json'
+            'Authorization'="Bearer " + $authResult.AccessToken
+            'ExpiresOn'=$authResult.ExpiresOn
+            }
+
+        return $authHeader
+
+        }
+
+        else {
+
+        Write-Host
+        Write-Host "Authorization Access Token is null, please re-run authentication..." -ForegroundColor Red
+        Write-Host
+        break
+
+        }
+
+    }
+
+    catch {
+
+    write-host $_.Exception.Message -f Red
+    write-host $_.Exception.ItemName -f Red
+    write-host
+    break
+
+    }
+
+}
+
+####################################################
+
+Function Get-EnterpriseDomain(){
+
+
+
+<#
+
+.SYNOPSIS
+
+This function is used to get the initial domain created using the Graph API REST interface
+
+.DESCRIPTION
+
+The function connects to the Graph API Interface and gets the initial domain created
+
+.EXAMPLE
+
+Get-EnterpriseDomain
+
+Gets the initial domain created in Azure - Format domain.onmicrosoft.com
+
+.NOTES
+
+NAME: Get-EnterpriseDomain
+
+#>
+
+
+
+    try {
+
+
+
+    $uri = "https://graph.microsoft.com/v1.0/domains"
+
+
+
+    $domains = (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+
+
+
+    $EnterpriseDomain = ($domains | ? { $_.isInitial -eq $true } | select id).id
+
+
+
+    return $EnterpriseDomain
+
+
+
+    }
+
+
+
+    catch {
+
+
+
+    $ex = $_.Exception
+
+    $errorResponse = $ex.Response.GetResponseStream()
+
+    $reader = New-Object System.IO.StreamReader($errorResponse)
+
+    $reader.BaseStream.Position = 0
+
+    $reader.DiscardBufferedData()
+
+    $responseBody = $reader.ReadToEnd();
+
+    Write-Host "Response content:`n$responseBody" -f Red
+
+    Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+
+    write-host
+
+    break
+
+
+
+
+
+    }
+
+
+
+}
+
+####################################################
+
+Function Get-TenantID(){
+
+
+
+<#
+
+.SYNOPSIS
+
+This function is used to get the tenant ID using the Graph API REST interface
+
+.DESCRIPTION
+
+The function connects to the Graph API Interface and gets the tenant ID
+
+.EXAMPLE
+
+Get-TenantID
+
+Gets the unique ID of this tenant in Azure - Format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+.NOTES
+
+NAME: Get-TenantID
+
+#>
+
+
+
+    try {
+
+
+
+    $uri = "https://graph.microsoft.com/v1.0/organization"
+    
+    $organziation = (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+    
+    $TenantID = ($organziation | select id).id
+    
+    return $TenantID
+    
+    }
+
+
+
+    catch {
+    
+    $ex = $_.Exception
+
+    $errorResponse = $ex.Response.GetResponseStream()
+
+    $reader = New-Object System.IO.StreamReader($errorResponse)
+
+    $reader.BaseStream.Position = 0
+
+    $reader.DiscardBufferedData()
+
+    $responseBody = $reader.ReadToEnd();
+
+    Write-Host "Response content:`n$responseBody" -f Red
+
+    Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+
+    write-host
+
+    break
+    
+
+    }
+
+
+
+}
+
+<# Summary of function:
+$uri = "https://graph.microsoft.com/v1.0/organization"
+$organziation = (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+$TenantID = ($organziation | select id).id
+return $TenantID
+#>
+
+
+####################################################
+
+Function Create-GroupPolicyConfigurations(){
+		
+<#
+.SYNOPSIS
+This function is used to add an device configuration policy using the Graph API REST interface
+.DESCRIPTION
+The function connects to the Graph API Interface and adds a device configuration policy
+.EXAMPLE
+Add-DeviceConfigurationPolicy -JSON $JSON
+Adds a device configuration policy in Intune
+.NOTES
+NAME: Add-DeviceConfigurationPolicy
+#>
+		
+		[cmdletbinding()]
+		param
+		(
+			$DisplayName,
+            $PolicyDescription
+		)
+		
+		$jsonCode = @"
+{
+    "description":"$($PolicyDescription)",
+    "displayName":"$($DisplayName)"
+}
+"@
+		
+		$graphApiVersion = "Beta"
+		$DCP_resource = "deviceManagement/groupPolicyConfigurations"
+		Write-Verbose "Resource: $DCP_resource"
+		
+		try
+		{
+			
+			$uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)"
+			$responseBody = Invoke-RestMethod -Uri $uri -Headers $authToken -Method Post -Body $jsonCode -ContentType "application/json"
+			
+			
+		}
+		
+		catch
+		{
+			
+			$ex = $_.Exception
+			$errorResponse = $ex.Response.GetResponseStream()
+			$reader = New-Object System.IO.StreamReader($errorResponse)
+			$reader.BaseStream.Position = 0
+			$reader.DiscardBufferedData()
+			$responseBody = $reader.ReadToEnd();
+			Write-Host "Response content:`n$responseBody" -f Red
+			Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+			write-host
+			break
+			
+		}
+		$responseBody.id
+}
+	
+####################################################
+	
+Function Create-GroupPolicyConfigurationsDefinitionValues(){
+		
+    <#
+    .SYNOPSIS
+    This function is used to get device configuration policies from the Graph API REST interface
+    .DESCRIPTION
+    The function connects to the Graph API Interface and gets any device configuration policies
+    .EXAMPLE
+    Get-DeviceConfigurationPolicy
+    Returns any device configuration policies configured in Intune
+    .NOTES
+    NAME: Get-GroupPolicyConfigurations
+    #>
+		
+		[cmdletbinding()]
+		Param (
+			
+			[string]$GroupPolicyConfigurationID,
+			$JSON
+			
+		)
+		
+		$graphApiVersion = "Beta"
+		
+		$DCP_resource = "deviceManagement/groupPolicyConfigurations/$($GroupPolicyConfigurationID)/definitionValues"
+		write-host $DCP_resource
+		try
+		{
+			if ($JSON -eq "" -or $JSON -eq $null)
+			{
+				
+				write-host "No JSON specified, please specify valid JSON for the Device Configuration Policy..." -f Red
+				
+			}
+			
+			else
+			{
+				
+				Test-JSON -JSON $JSON
+				
+				$uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)"
+				Invoke-RestMethod -Uri $uri -Headers $authToken -Method Post -Body $JSON -ContentType "application/json"
+			}
+			
+		}
+		
+		catch
+		{
+			
+			$ex = $_.Exception
+			$errorResponse = $ex.Response.GetResponseStream()
+			$reader = New-Object System.IO.StreamReader($errorResponse)
+			$reader.BaseStream.Position = 0
+			$reader.DiscardBufferedData()
+			$responseBody = $reader.ReadToEnd();
+			Write-Host "Response content:`n$responseBody" -f Red
+			Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+			write-host
+			break
+			
+		}
+		
+}
+	
+####################################################
+
+Function Test-JSON(){
+		
+<#
+.SYNOPSIS
+This function is used to test if the JSON passed to a REST Post request is valid
+.DESCRIPTION
+The function tests if the JSON passed to the REST Post is valid
+.EXAMPLE
+Test-JSON -JSON $JSON
+Test if the JSON is valid before calling the Graph REST interface
+.NOTES
+NAME: Test-AuthHeader
+#>
+		
+		param (
+			
+			$JSON
+			
+		)
+		
+		try
+		{
+			
+			$TestJSON = ConvertFrom-Json $JSON -ErrorAction Stop
+			$validJson = $true
+			
+		}
+		
+		catch
+		{
+			
+			$validJson = $false
+			$_.Exception
+			
+		}
+		
+		if (!$validJson)
+		{
+			
+			Write-Host "Provided JSON isn't in valid JSON format" -f Red
+			break
+			
+		}
+		
+}
+
+####################################################
+
+
+
+###################################################################################################
+## Connect to the graph in order to run the script
+###################################################################################################
+
+#region Authentication
+
+write-host
+
+# Checking if authToken exists before running authentication
+if($global:authToken){
+
+    # Setting DateTime to Universal time to work in all timezones
+    $DateTime = (Get-Date).ToUniversalTime()
+
+    # If the authToken exists checking when it expires
+    $TokenExpires = ($authToken.ExpiresOn.datetime - $DateTime).Minutes
+
+        if($TokenExpires -le 0){
+
+        write-host "Authentication Token expired" $TokenExpires "minutes ago" -ForegroundColor Yellow
+        write-host
+
+            # Defining User Principal Name if not present
+
+            if($User -eq $null -or $User -eq ""){
+
+            $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+            Write-Host
+
+            }
+
+        $global:authToken = Get-AuthToken -User $User
+
+        }
+}
+
+# Authentication doesn't exist, calling Get-AuthToken function
+
+else {
+
+    if($User -eq $null -or $User -eq ""){
+
+    $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+    Write-Host
+
+    }
+
+# Getting the authorization token
+$global:authToken = Get-AuthToken -User $User
+
+}
+
+#endregion
+
+####################################################
+
+## Get the TenantID for use in OneDrive Configuration profiles
+$TenantID = Get-TenantID
+
+###################################################################################################
+## JSON describing the policies to be imported
+###################################################################################################
+
+$GPO_ODfBClient = @"
+[
+{
+    "definition@odata.bind":  "https://graph.microsoft.com/beta/deviceManagement/groupPolicyDefinitions('81c07ba0-7512-402d-b1f6-00856975cfab')",
+    "enabled":  "true"
+},
+{
+    "definition@odata.bind":  "https://graph.microsoft.com/beta/deviceManagement/groupPolicyDefinitions('61b07a01-7e60-4127-b086-f6b32458a5c5')",
+    "enabled":  "true"
+},
+{
+    "definition@odata.bind":  "https://graph.microsoft.com/beta/deviceManagement/groupPolicyDefinitions('2ce2f507-aae8-49a1-98ce-dc3faafcd331')",
+    "enabled":  "true"
+},
+{
+    "definition@odata.bind":  "https://graph.microsoft.com/beta/deviceManagement/groupPolicyDefinitions('24bec81e-a596-4fc1-867b-0256114a19a4')",
+    "enabled":  "true",
+    "presentationValues":  [
+                               {
+                                   "@odata.type":  "#microsoft.graph.groupPolicyPresentationValueText",
+                                   "value":  "$TenantID",
+                                   "presentation@odata.bind":  "https://graph.microsoft.com/beta/deviceManagement/groupPolicyDefinitions('24bec81e-a596-4fc1-867b-0256114a19a4')/presentations('4640c3f7-b89a-41be-948b-e86a2a97cae4')"
+                               }
+                           ]
+},
+{
+    "definition@odata.bind":  "https://graph.microsoft.com/beta/deviceManagement/groupPolicyDefinitions('39147fa2-6c5e-437b-8264-19b50b891709')",
+    "enabled":  "true",
+    "presentationValues":  [
+                               {
+                                   "@odata.type":  "#microsoft.graph.groupPolicyPresentationValueText",
+                                   "value":  "$TenantID",
+                                   "presentation@odata.bind":  "https://graph.microsoft.com/beta/deviceManagement/groupPolicyDefinitions('39147fa2-6c5e-437b-8264-19b50b891709')/presentations('fbefbbdf-5382-477c-8b6c-71f4a06e2805')"
+                               }
+                           ]
+}
+]
+"@
+
+
+###################################################################################################
+## Import the ADMX Configuration profiles from JSON
+###################################################################################################
+
+## Create the ADMX Configuration profile
+$Policy_Name = "[ITPM Corporate] Windows 10: OneDrive client config"
+$Policy_Description = "Configures the OneDrive client. Assign this profile to Company owned Windows 10 devices."
+$GroupPolicyConfigurationID = Create-GroupPolicyConfigurations -DisplayName $Policy_Name -PolicyDescription $Policy_Description
+## Populate the policy with settings
+$JSON_Convert = $GPO_ODfBClient | ConvertFrom-Json
+$JSON_Convert | ForEach-Object { $_
+    
+            	$JSON_Output = ConvertTo-Json -Depth 5 $_
+                
+                Write-Host $JSON_Output
+
+            	Create-GroupPolicyConfigurationsDefinitionValues -JSON $JSON_Output -GroupPolicyConfigurationID $GroupPolicyConfigurationID 
+        	 }
+
+
+
+Write-Host "####################################################################################################" -ForegroundColor Yellow
+Write-Host "Policy: " $Policy_Name "created" -ForegroundColor Yellow
+Write-Host "####################################################################################################" -ForegroundColor Yellow
