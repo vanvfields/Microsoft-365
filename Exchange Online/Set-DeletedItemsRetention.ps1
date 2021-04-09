ef47261f636d630f6f726299fd1292ff226f0de5
Alex Fields <50560275+vanvfields@users.noreply.github.com>
2021-04-09 10:55:21 -0600

    Update Set-DeletedItemsRetention.ps1

--- Exchange Online/Set-DeletedItemsRetention.ps1
+++ Exchange Online/Set-DeletedItemsRetention.ps1
@@ -11,6 +11,10 @@ $MessageColor = "cyan"
 $AssessmentColor = "magenta"
 
 Write-Host 
+$CurrentRetention = (Get-Mailbox -ResultSize Unlimited).RetainDeletedItemsFor
+Write-Host -ForegroundColor $AssessmentColor "Current retention limit (in days and number of mailboxes):"
+$CurrentRetention | group | select name, count | ft
+Write-Host 
 $Answer = Read-Host "By default Exchange Online retains deleted items for 14 days; would you like to enforce the maximum allowed value of 30 days for all mailboxes? Type Y or N and press Enter to continue"
 if ($Answer -eq 'y' -or $Answer -eq 'yes') {
     Get-Mailbox -ResultSize Unlimited | Set-Mailbox -RetainDeletedItemsFor 30
