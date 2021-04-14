e1b698c4c5bde345abdfa5b2a50acdef9ca69c92
Alex Fields <50560275+vanvfields@users.noreply.github.com>
2021-04-13 18:51:11 -0600

    Add files via upload

--- Azure AD/Enable-SensitivityLabelsForGroups.ps1
+++ Azure AD/Enable-SensitivityLabelsForGroups.ps1
@@ -12,7 +12,10 @@
     FileName:    Enable-SensitivityLabelsForGroups.ps1
     Author:      Alex Fields, ITProMentor.com
     Created:     November 2020
-	Updated:     November 2020
+	Updated:     April 2021
+
+.NOTES
+    You will be prompted twice since you must authenticate against both Azure AD and Exchange Online
 
 #>
 ###################################################################################################
