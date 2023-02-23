7317f8bb9621ab1421fb8ec4b1614e22daa6c775
Alex Fields <50560275+vanvfields@users.noreply.github.com>
2023-02-23 15:31:18 -0600

    Add files via upload

--- Incident Response/Install-AzureADProtectionAlerts.ps1
+++ Incident Response/Install-AzureADProtectionAlerts.ps1
@@ -12,45 +12,244 @@
     Created:     June 2021
 	Revised:     June 2021
     
+
+.EXAMPLE 
+    .\Install-AzureADProtectionAlerts.ps1 -AlertingEmail "alerts@contoso.com" 
+
+
 #>
 ###################################################################################################
 
-## Import EXO management module and connect to Security & Compliance Center
-Import-Module ExchangeOnlineManagement
-Connect-IPPSSession
 
-$notifyUser = ""
+#region parameters
+Param(
+    [Parameter(Mandatory=$True)]
+    [System.String]$AlertingEmail
+)
 
-if (!$notifyUser) {
-$notifyUser = Read-Host "Enter the email address at which you will recieve alerts."
+if (!$AlertingEmail) {
+$AlertingEmail = Read-Host "Enter the email address at which you will recieve alerts."
 Write-Host
 }
 
-## Administrative roles are modified
-New-ProtectionAlert -Name "New member added to an Azure AD Role" -Category Others -NotifyUser $notifyUser -ThreatType Activity -Operation "Add member to role." -Description "Custom alert policy to track when members are added to Azure AD Roles" -AggregationType None
+#endregion
 
-## New applications are added to the directory (always investigate unknown or unexpected apps)
-New-ProtectionAlert -Name "New application added to the directory" -Category Others -NotifyUser $notifyUser -ThreatType Activity -Operation "Add application." -Description "Custom alert policy to track when applications are added to the environment" -AggregationType None
+<#region connect to the security & compliance center
 
-## New client credentials added to an application or service principal (could indicate app abuse if otherwise unexpected)
-New-ProtectionAlert -Name "New client credentials added to an application" -Category Others -NotifyUser $notifyUser -ThreatType Activity -Operation "Update application – Certificates and secrets" -Description "Custom alert policy to track when certificates or secrets are added to an application" -AggregationType None
+Import-Module ExchangeOnlineManagement
+Connect-IPPSSession
 
-## Adding either delegated permissions or application permissions, and new consent grants (could indicate app abuse if otherwise unexpected)
-New-ProtectionAlert -Name "Add app role assignment to service principal" -Category Others -NotifyUser $notifyUser -ThreatType Activity -Operation "Add app role assignment to service principal." -Description "Custom alert policy to track when new application permissions are added" -AggregationType None
-New-ProtectionAlert -Name "Add delegated permission grant" -Category Others -NotifyUser $notifyUser -ThreatType Activity -Operation "Add delegated permission grant." -Description "Custom alert policy to track when new delegated permissions are added" -AggregationType None
-New-ProtectionAlert -Name "Consent to application" -Category Others -NotifyUser $notifyUser -ThreatType Activity -Operation "Consent to application." -Description "Custom alert policy to track when application consent is granted for new permissions." -AggregationType None
+#endregion
+#>
 
-## Federation settings are modified or partners are added to the organization (if these events are unexpected, it could mean access is being granted to outside domains or partners)
-New-ProtectionAlert -Name "Set federation settings on domain" -Category Others -NotifyUser $notifyUser -ThreatType Activity -Operation "Set federation settings on domain." -Description "Custom alert policy to track when domain federation settings have been modified" -AggregationType None
-New-ProtectionAlert -Name "Partner relationship added to the organization" -Category Others -NotifyUser $notifyUser -ThreatType Activity -Operation "Add partner to company." -Description "Custom alert policy to track when partner relationships have been added to the tenant" -AggregationType None
+#region add alerts
 
-## Conditional Access policy modifications (attackers may want to bypass CA policies, always investigate unexpected changes)
-New-ProtectionAlert -Name "Conditional Access policy added" -Category Others -NotifyUser $notifyUser -ThreatType Activity -Operation "Add policy." -Description "Custom alert policy to track when Conditional Access policies are created in Azure AD" -AggregationType None
-New-ProtectionAlert -Name "Conditional Access policy updated" -Category Others -NotifyUser $notifyUser -ThreatType Activity -Operation "Update policy." -Description "Custom alert policy to track when Conditional Access policies are updated in Azure AD" -AggregationType None
-New-ProtectionAlert -Name "Conditional Access policy deleted" -Category Others -NotifyUser $notifyUser -ThreatType Activity -Operation "Delete policy." -Description "Custom alert policy to track when Conditional Access policies are deleted in Azure AD" -AggregationType None
+        ## Alert when new members are added to Azure AD roles 
+            $RawName = "New member added to a role"
+            $NewName = if ($RawName.Length -gt 30) { "$($RawName.substring(0,30)) - $($AlertingEmail)" } else { "$($RawName) - $($AlertingEmail)" }
+            $splat = @{
+                name                = $NewName
+                NotifyUser          = $AlertingEmail
+                Operation           = "Add member to role."
+                NotificationEnabled = $true
+                Severity            = "Informational"
+                Category            = "AccessGovernance"
+                Comment             = "A new member has been added to a role in Azure AD."
+                threattype          = "Activity"
+                AggregationType     = "None"
+                Disabled            = $false
+            }
+            try {
+              $null = New-ProtectionAlert @splat -ErrorAction Stop
+            }
+            catch {
+                write-host "Could not create rule. Error: $($_.Exception.Message)"
+            }
+            
+        ## Alert when new applications are added to the directory 
+            $RawName = "New application added"
+            $NewName = if ($RawName.Length -gt 30) { "$($RawName.substring(0,30)) - $($AlertingEmail)" } else { "$($RawName) - $($AlertingEmail)" }
+            $splat = @{
+                name                = $NewName
+                NotifyUser          = $AlertingEmail
+                Operation           = "Add application."
+                NotificationEnabled = $true
+                Severity            = "Informational"
+                Category            = "AccessGovernance"
+                Comment             = "An application was added to Azure AD."
+                threattype          = "Activity"
+                AggregationType     = "None"
+                Disabled            = $false
+            }
+            try {
+              $null = New-ProtectionAlert @splat -ErrorAction Stop
+            }
+            catch {
+                write-host "Could not create rule. Error: $($_.Exception.Message)"
+            }
+
+
+        ## Alert when new client credentials are added to an application or service principal 
+            $RawName = "Credentials added to an application"
+            $NewName = if ($RawName.Length -gt 30) { "$($RawName.substring(0,30)) - $($AlertingEmail)" } else { "$($RawName) - $($AlertingEmail)" }
+            $splat = @{
+                name                = $NewName
+                NotifyUser          = $AlertingEmail
+                Operation           = "Update application – Certificates and secrets"
+                NotificationEnabled = $true
+                Severity            = "Informational"
+                Category            = "AccessGovernance"
+                Comment             = "Certificate or secret was added to an application."
+                threattype          = "Activity"
+                AggregationType     = "None"
+                Disabled            = $false
+            }
+            try {
+              $null = New-ProtectionAlert @splat -ErrorAction Stop
+            }
+            catch {
+                write-host "Could not create rule. Error: $($_.Exception.Message)"
+            }
+
+        ## Alert when new application consent is granted 
+            $RawName = "Application consent granted"
+            $NewName = if ($RawName.Length -gt 30) { "$($RawName.substring(0,30)) - $($AlertingEmail)" } else { "$($RawName) - $($AlertingEmail)" }
+            $splat = @{
+                name                = $NewName
+                NotifyUser          = $AlertingEmail
+                Operation           = "Consent to application."
+                NotificationEnabled = $true
+                Severity            = "Informational"
+                Category            = "AccessGovernance"
+                Comment             = "New permissions consent granted for an application."
+                threattype          = "Activity"
+                AggregationType     = "None"
+                Disabled            = $false
+            }
+            try {
+              $null = New-ProtectionAlert @splat -ErrorAction Stop
+            }
+            catch {
+                write-host "Could not create rule. Error: $($_.Exception.Message)"
+            }
+
+
+        ## Alert when federation settings are modified 
+            $RawName = "New federation settings"
+            $NewName = if ($RawName.Length -gt 30) { "$($RawName.substring(0,30)) - $($AlertingEmail)" } else { "$($RawName) - $($AlertingEmail)" }
+            $splat = @{
+                name                = $NewName
+                NotifyUser          = $AlertingEmail
+                Operation           = "Set federation settings on domain."
+                NotificationEnabled = $true
+                Severity            = "Informational"
+                Category            = "AccessGovernance"
+                Comment             = "Domain federation settings have been modified."
+                threattype          = "Activity"
+                AggregationType     = "None"
+                Disabled            = $false
+            }
+            try {
+              $null = New-ProtectionAlert @splat -ErrorAction Stop
+            }
+            catch {
+                write-host "Could not create rule. Error: $($_.Exception.Message)"
+            }
+
+
+        ## Alert when partner settings are modified 
+            $RawName = "Partner relationship added"
+            $NewName = if ($RawName.Length -gt 30) { "$($RawName.substring(0,30)) - $($AlertingEmail)" } else { "$($RawName) - $($AlertingEmail)" }
+            $splat = @{
+                name                = $NewName
+                NotifyUser          = $AlertingEmail
+                Operation           = "Add partner to company."
+                NotificationEnabled = $true
+                Severity            = "Informational"
+                Category            = "AccessGovernance"
+                Comment             = "Partner relationship has been added to the tenant."
+                threattype          = "Activity"
+                AggregationType     = "None"
+                Disabled            = $false
+            }
+            try {
+              $null = New-ProtectionAlert @splat -ErrorAction Stop
+            }
+            catch {
+                write-host "Could not create rule. Error: $($_.Exception.Message)"
+            }
+
+
+        ## Alert when CA policy is modified 
+            $RawName = "CA Policy has been updated"
+            $NewName = if ($RawName.Length -gt 30) { "$($RawName.substring(0,30)) - $($AlertingEmail)" } else { "$($RawName) - $($AlertingEmail)" }
+            $splat = @{
+                name                = $NewName
+                NotifyUser          = $AlertingEmail
+                Operation           = "Update policy."
+                NotificationEnabled = $true
+                Severity            = "Informational"
+                Category            = "AccessGovernance"
+                Comment             = "A Conditional Access policy has been updated."
+                threattype          = "Activity"
+                AggregationType     = "None"
+                Disabled            = $false
+            }
+            try {
+              $null = New-ProtectionAlert @splat -ErrorAction Stop
+            }
+            catch {
+                write-host "Could not create rule. Error: $($_.Exception.Message)"
+            }
+
+        ## Alert when CA policy is added 
+            $RawName = "CA Policy has been added"
+            $NewName = if ($RawName.Length -gt 30) { "$($RawName.substring(0,30)) - $($AlertingEmail)" } else { "$($RawName) - $($AlertingEmail)" }
+            $splat = @{
+                name                = $NewName
+                NotifyUser          = $AlertingEmail
+                Operation           = "Add policy."
+                NotificationEnabled = $true
+                Severity            = "Informational"
+                Category            = "AccessGovernance"
+                Comment             = "A new Conditional Access policy has been added."
+                threattype          = "Activity"
+                AggregationType     = "None"
+                Disabled            = $false
+            }
+            try {
+              $null = New-ProtectionAlert @splat -ErrorAction Stop
+            }
+            catch {
+                write-host "Could not create rule. Error: $($_.Exception.Message)"
+            }
+
+        ## Alert when CA policy is deleted 
+            $RawName = "CA Policy has been deleted"
+            $NewName = if ($RawName.Length -gt 30) { "$($RawName.substring(0,30)) - $($AlertingEmail)" } else { "$($RawName) - $($AlertingEmail)" }
+            $splat = @{
+                name                = $NewName
+                NotifyUser          = $AlertingEmail
+                Operation           = "Delete policy."
+                NotificationEnabled = $true
+                Severity            = "Informational"
+                Category            = "AccessGovernance"
+                Comment             = "A Conditional Access policy has been deleted."
+                threattype          = "Activity"
+                AggregationType     = "None"
+                Disabled            = $false
+            }
+            try {
+              $null = New-ProtectionAlert @splat -ErrorAction Stop
+            }
+            catch {
+                write-host "Could not create rule. Error: $($_.Exception.Message)"
+            }
+
+
 
 Write-Host
 Write-Host "Script completed." -ForegroundColor Cyan
-Write-Host 
-Write-Host "You should also edit the email address for the default protection alerts at: https://protection.office.com/alertpolicies" -ForegroundColor Cyan
 Write-Host
+
+#endregion
